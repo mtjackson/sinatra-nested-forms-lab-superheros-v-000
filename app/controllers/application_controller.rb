af9b57c4f4b81df6bceeb628987655ca6ccce8ce
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-18 11:52:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
-    pots '/teams' do
+    post '/teams' do
       @team = Team.new(params[:team])
 
       params[:team][:heroes].each do |details|
